76ab3b00c849407f17871d1c10f0da5855a32d64
Jose Fernandez <josef@netflix.com>
2020-05-26 21:45:33 -0700

    bootstrap app

---
+++ src/main/java/com/jrfernandez/reactorplayground/ReactorplaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.jrfernandez.reactorplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorplaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorplaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrfernandez/reactorplayground/ReactorplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrfernandez.reactorplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
